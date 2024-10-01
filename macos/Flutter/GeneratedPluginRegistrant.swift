b3df895818b613276fbc803f54530e02b72bb268
LilithZ007 <thitiwat.tiw17@gmail.com>
2024-10-01 15:55:28 +0700

    ..

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,9 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import file_selector_macos
-import geolocator_apple
-import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
 }
